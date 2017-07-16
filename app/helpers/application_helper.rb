28f06e7e73761406e8c8d093ebd102930862aa8c
Andres de Lago Gomez <delagoandres@gmail.com>
2017-07-16 14:54:19 -0500

    Modifies helper to make it easier and more consitent to use

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-  def sidebar_link icon, text, link, options=nil
+  def sidebar_link icon, text, link=nil, **options
     content_tag :li, options do
       link_to(link ? link : 'javascript:void(0)') do
-        fa_icon(icon) + content_tag(:p, text)
+        fa_icon(icon, class: 'fa-fw') + content_tag(:p, text)
       end
     end
   end
